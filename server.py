3b06a1a03ae2a4920b7c84181b20506e85caa008
avaldezs <avaldez@mail.umw.edu>
2018-10-11 18:46:45 -0400

    added changes to the user.py create function.

--- server.py
+++ server.py
@@ -15,13 +15,26 @@ def home():
     return render_template('home.html', page_title='gusty.bike')
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+
+    if request.method == "POST":
+        if not request.form["email"] or not request.form["username"] or not request.form["password"]:
+            return '0'
+
+        email = request.form["email"]
+        username = request.form["username"]
+        pw = request.form["password"]
+
+        if user.create(email, pw, username, "gusty_bike"):
+            return '1'
+        else:
+            return '0'
 
     return render_template('register.html', page_title='gusty.bike')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
 
     return render_template('login.html', page_title='gusty.bike')
